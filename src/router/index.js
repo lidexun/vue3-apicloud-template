cb25af6c2120204f561aebc6da4abee13a5048e8
lidexun <910547462@qq.com>
2022-06-08 21:03:18 +0800

    WebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/home/index.vue'
 
 const routes = [
   {
@@ -14,13 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import(/* webpackChunkName: "about" */ '../views/about/index.vue')
     }
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../../assets/logo.png" />
+    <button @click="openwin">openwin</button>
+  </div>
+</template>
+
+<script>
+import utils from '../../utils/utils.js'
+export default {
+  name: 'HomeView',
+  methods: {
+    openwin() {
+      utils.openWin({
+        url: '/about'
+      })
+    }
+  }
+}
+</script>
