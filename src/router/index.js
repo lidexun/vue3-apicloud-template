3f81a5a72b8d550d0a78ebb347c150d53fe2e2c1
lidexun <910547462@qq.com>
2022-06-12 01:43:30 +0800

    调整路由结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/home/index.vue'
 
 const routes = [
   {
     path: '/',
+    redirect: 'main'
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: () => import('../views/main/index.vue')
+  },
+  {
+    path: '/home',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/home/index.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/about/index.vue')
-    }
+    component: () => import('../views/about/index.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
