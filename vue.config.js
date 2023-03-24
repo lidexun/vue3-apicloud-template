0e5ebd5c5da52c85097f4b60ae7154b86e58ab60
lidexun <910547462@qq.com>
2023-03-25 02:14:12 +0800

    优化配置

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 const AutoImport = require('unplugin-auto-import/webpack')
 const { VantResolver } = require('unplugin-vue-components/resolvers')
 const ComponentsPlugin = require('unplugin-vue-components/webpack')
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const { createConfig, IP } = require('./build/index.js')
-createConfig()
-process.env.VUE_APP_PATH = 'http://' + IP() + '/#'
+if (process.env.npm_lifecycle_event === 'serve') {
+  createConfig()
+  process.env.VUE_APP_PATH = 'http://' + IP() + '/#'
+}
+
 module.exports = defineConfig({
   outputDir: 'apicloud/widget',
   publicPath: './',
@@ -17,7 +19,6 @@ module.exports = defineConfig({
   },
   configureWebpack: {
     plugins: [
-      new CompressionWebpackPlugin(),
       // 按需导入vant组件
       ComponentsPlugin({
         resolvers: [VantResolver()]
